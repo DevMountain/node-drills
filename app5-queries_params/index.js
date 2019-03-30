f57d74d552395c45c948f83632e906d42753426b
Steven Isbell <steveni_92@msn.com>
2019-03-29 20:31:53 -0600

    Update index.js

--- app5-queries_params/index.js
+++ app5-queries_params/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var port = 3000;
 var data = require('./data.js')
 
 var app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 
 
